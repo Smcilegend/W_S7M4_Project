404296ad44067f5bfbb851b26d316dbffd00fe65
Michael Gibson <selfmadecultureinc@gmail.com>
2025-01-15 01:36:03 -0600

    Update_Model_4_Project

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -10,35 +10,62 @@ describe('Module 4 Project Tests', () => {
 
       One test is done for you as an example.
     */
-    test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
-      render(<App lang="en" />)
-      expect(screen.getByText("Create an Account")).toBeVisible()
+   let texts = getEntriesByKeyPrefix(txt.en, 'TEXT')
+   for (let [key, val] of texts) {
+    test(`${key} is visible`, () => {
+      render(<App />)
+      expect(screen. getByText(val)).toBeVisible()
+    })
+   }
+   let labels = getEntriesByKeyPrefix(txt.en, 'LABEL')
+   for (let [key, val] of labels) {
+    test(`${key} is visible`, () => {
+      render(<App />)
+      expect(screen. getByLabelText(val)).toBeVisible()
+    })
+   }
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByPlaceholderText(txt.esp.PLACEHOLDER_USERNAME)).toBeVisible()
     })
   })
   describe('Spanish Language', () => {
-    /*
-      👉 TASK 3
-
-      This is done after making the UI multilingual.
-    */
+    test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(txt.esp.TEXT_HEADING_CREATE_ACCOUNT)).toBeVisible()
+    })
+    test(`LABEL_USERNAME is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByLabelText(txt.esp.LABEL_USERNAME)).toBeVisible()
+    })
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByPlaceholderText(txt.esp.PLACEHOLDER_USERNAME)).toBeVisible()
+    })
   })
   describe('getEntriesByKeyPrefix', () => {
     test('can extract the correct data', () => {
-    /*
-      👉 TASK 4 part 2
-
-      Implement the function `getEntriesByKeyPrefix` below
-      and then come back here and write a few tests
-      to ensure it works as expected.
-
-      Although it should be noted that commonly,
-      the tests are written _before_ implementing
-      the function being tested.
-    */
+      const obj = { abc_1: "data_abc_1",
+        abc_2: "data_abc_2",
+        xyz_1: "data_xyz_1",
+        abc_3: "data_abc_3",
+      }
+      const expected = [
+        ["abc_1", "data_abc_1"],
+        ["abc_2", "data_abc_2"],
+        ["abc_3", "data_abc_3"],
+      ]
+      const expected2 = [
+        ["xyz_1", "data_xyz_1"],
+      ]
+      expect(getEntriesByKeyPrefix(obj, "abc")).toEqual(expected)
+      expect(getEntriesByKeyPrefix(obj, "xyp")).toEqual(expected2)
+      expect(getEntriesByKeyPrefix(obj, "foo")).toEqual([])
     })
   })
 })
 function getEntriesByKeyPrefix(obj, keyPrefix) {
+  return Object.entries(obj).filter(([key, val]) => key.split('_')[0] === keyPrefix)
   /*
     👉 TASK 4 part 1
 
@@ -55,17 +82,11 @@ function getEntriesByKeyPrefix(obj, keyPrefix) {
 
     and returns an array of arrays such as this (for the arguments given in the examples above):
 
-    [
-      ["abc_1", "data_abc_1"],
-      ["abc_2", "data_abc_2"],
-      ["abc_3", "data_abc_3"],
-    ]
+    
 
     If the function is passed the same `obj` as above but a `keyPrefix` of "xyz" then it would return:
 
-    [
-      ["xyz_1", "data_xyz_1"],
-    ]
+    
 
     If the function is passed the same `obj` as above but a `keyPrefix` of "foo" then it would return the empty array.
 
